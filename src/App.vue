db725dd43e8f90c3052cde03ccf0917fa1694dd6
elenakout <elenakout@gmail.com>
2023-10-12 11:15:04 +0300

    feat: todo list

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,13 @@
 
   </form>
   <h2>ToDo List</h2>
-  <ul>
+  <AppList @doneTodo="doneTodo" @removeTodo="removeTodo" :todos="todos"/>
+  <!-- <ul>
     <li v-for="(todo, index) in todos" :key="index">
       <span :class="{ done: todo.done }" @click="doneTodo(todo)">{{ todo.content }}</span>
       <button @click="removeTodo(index)">Remove</button>
     </li>
-  </ul>
+  </ul> -->
   <h4 v-if="todos.length === 0">Empty list.</h4>
 </template>
 
@@ -20,6 +21,7 @@
 import { ref } from 'vue';
 import AppInput from './components/AppInput.vue';
 import AppButton from './components/AppButton.vue'
+import AppList from './components/AppList.vue';
 
 
 const newTodo = ref('');
@@ -45,8 +47,8 @@ const addTodo = () => {
   saveData();
 };
 
-const doneTodo = (todo) => {
-  todo.done = !todo.done;
+  const doneTodo = (todo) => {
+    todo.done = !todo.done;
   saveData();
 };
 
@@ -63,24 +65,6 @@ const saveData = () => {
 </script>
 
 <style >
-body {
-  margin: 0;
-  padding: 0;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background-color: #27292d;
-  color: white;
-}
-
-#app {
-
-  max-width: 600px;
-  margin-left: auto;
-  margin-right: auto;
-  padding: 20px;
-}
-
 h1 {
   font-weight: bold;
   font-size: 28px;
@@ -93,72 +77,12 @@ form {
   width: 100%;
 }
 
-/* form label {
-  font-size: 14px;
-  font-weight: bold;
-} */
-
-/* form input, */
-/* body #app form button {
-  height: 48px;
-  box-shadow: none;
-  outline: none;
-  padding-left: 12px;
-  padding-right: 12px;
-  border-radius: 6px;
-  font-size: 18px;
-  margin-top: 6px;
-  margin-bottom: 12px;
-} */
-
-/* form input {
-  background-color: transparent;
-  border: 2px solid rgba(255, 255, 255, 0.35);
-  color: inherit;
-} */
-
-/* button {
-  cursor: pointer;
-  background-color: #a0a4d9;
-  border: 1px solid #a0a4d9;
-  color: #1f2023;
-  font-weight: bold;
-  outline: none;
-  border-radius: 6px;
-} */
-
 h2 {
   font-size: 22px;
   border-bottom: 2px solid rgba(255, 255, 255, 0.35);
   padding-bottom: 6px;
 }
 
-ul {
-  padding: 10px;
-}
-
-ul li {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 2px solid rgba(255, 255, 255, 0.35);
-  padding: 12px 24px;
-  border-radius: 6px;
-  margin-bottom: 12px;
-}
-
-ul li span {
-  cursor: pointer;
-}
-
-ul li .done {
-  text-decoration: line-through;
-}
-
-ul li button {
-  font-size: 12px;
-  padding: 6px;
-}
 
 h4 {
   text-align: center;
---
+++ src/components/AppList.vue
@@ -0,0 +1,52 @@
+<template>
+ <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      <span :class="{ done: todo.done }" @click="$emit('doneTodo',todo)">{{ todo.content }}</span>
+
+			<AppButton @buttonClicked="$emit('removeTodo', index)">Remove</AppButton>
+      <!-- <button @click="removeTodo(index)">Remove</button> -->
+    </li>
+  </ul>
+</template>
+
+<script setup>
+import AppButton from './AppButton.vue';
+
+defineProps({
+	todos: {
+		type:  Array,
+		required: true
+	}
+})
+
+</script>
+
+<style>
+ul {
+  padding: 10px;
+}
+
+ul li {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: 2px solid rgba(255, 255, 255, 0.35);
+  padding: 12px 24px;
+  border-radius: 6px;
+  margin-bottom: 12px;
+}
+
+ul li span {
+  cursor: pointer;
+}
+
+ul li .done {
+  text-decoration: line-through;
+}
+
+ul li button {
+  font-size: 12px;
+  padding: 6px;
+}
+
+</style>
